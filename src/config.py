b8b9cc26d0796bad8e49fa67a03fac1dada672ba
Alex <zhmurik@yandex.ru>
2020-04-26 18:20:05 -0400

    initial migration

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 config = {
     'debug': os.getenv('DEBUG', 'True').lower() == 'true',
-    'sql_db_url': os.getenv('DATABASE_URL', 'postgres://master:local_dev@localhost:5432/lead_collector'),
+    'sql_db_url': os.getenv('DATABASE_URL', 'postgres://master:local_dev@localhost:5432/lead_zeppelin'),
     'mongo_web_uri': os.getenv('MONGO_WEB_URL', 'mongodb://localhost:27017/lead_zeppelin_dev'),
     'mongo_jobs_uri': os.getenv('MONGO_JOBS_URL', 'mongodb://localhost:27017/lead_zeppelin_dev_jobs'),
     'redis_uri': os.getenv('REDIS_URL', 'redis://localhost:6379/0'),
